8c22da6070fd19d9d42fa91196e04748c3bfa7bb
Jala Unterreiner <jala.binbek@outlook.com>
2022-07-28 15:28:28 -0400

    rewrote revolvers for graphql syntax

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,47 +1,61 @@
-const {User} = require('../models');
+const { AuthenticationError } = require("apollo-server-errors");
+const { User } = require('../models');
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
-    async getSingleUser(parent, { user = null, params }) {
-      const foundUser = await User.findOne({
-        $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
-      });
-  
-        return foundUser;
-    },
+    getSingleUser: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError('Please log in!');
+    }
   },
   Mutation: {
-    async createUser(parent, { body }) {
-      const user = await User.create(body);
-      const token = signToken(user);
-      return({ token, user });
-    }, 
-    async login(parent, { body }) {
-      const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
-  
-      const correctPw = await user.isCorrectPassword(body.password);
+    createUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
       const token = signToken(user);
       return { token, user };
     },
-    async saveBook(parent, { user, body }) {
-      console.log(user);
-        const updatedUser = await User.findOneAndUpdate(
-          { _id: user._id },
-          { $addToSet: { savedBooks: body } },
-          { new: true, runValidators: true }
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError('Email address not found!');
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, { authors, description, bookId, image, link, title }, context) => {
+      if (context.user) {
+        const user = await User.findOneAndUpdate({ _id: context.user._id }, { $addToSet: {
+            savedBooks: {
+                authors: authors,
+                description: description,
+                bookId: bookId,
+                image: image,
+                link: link,
+                title: title
+            }
+        }});
+        return user;;
+    }
+    throw new AuthenticationError('Please login to view this content!');
+  },
+    deleteBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const user = await User.findOneAndUpdate(
+            { _id: context.user._id },
+            { $pull: {savedBooks: {bookId: bookId} } }
         );
-        return (updatedUser);
-      
-    },
-    async deleteBook(parent, { user, params }) {
-      const updatedUser = await User.findOneAndUpdate(
-        { _id: user._id },
-        { $pull: { savedBooks: { bookId: params.bookId } } },
-        { new: true }
-      ); 
-      return (updatedUser);
-    },
+      return User;
+    }
+    throw new AuthenticationError('Please login to view this content!');
   }
 }
+};
 
 module.exports = resolvers;
