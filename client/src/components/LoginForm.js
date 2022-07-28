a84269cb4f7e28b894aa3ffa8376f8538e9b4ac9
Jala Unterreiner <jala.binbek@outlook.com>
2022-07-28 18:04:17 -0400

    imported apollo and created apollo provider in app js

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from '@apollo/client';
-import { loginUser } from '../utils/API';
+// import { loginUser } from '../utils/API';
 import { LOGIN_USER } from '../utils/mutations';
 
 import Auth from '../utils/auth';
@@ -30,7 +30,7 @@ const LoginForm = () => {
 
     try {
       const {data} = await login({
-        variables: {userFormData}
+        variables: {...userFormData}
       });
       Auth.login(data.login.token);
     } catch (err) {
