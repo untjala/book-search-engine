5b9abd2cce9a739d9dd62cb3abae33c815456c4e
Jala Unterreiner <jala.binbek@outlook.com>
2022-07-28 15:33:13 -0400

    renamed getsingleuser, wrote types for user, books and auth

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,23 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type User {
+  _id: ID
+  username: String
+  email: String
+  password: String
+  savedBooks: [Book]!
+  bookCount: Int
+}
+
+type Book {
+        _id: ID
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
+
+`
