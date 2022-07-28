3169a884a97189f1c55f04effd87b5135331daae
Jala Unterreiner <jala.binbek@outlook.com>
2022-07-28 15:46:32 -0400

    finished typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -19,5 +19,19 @@ type Book {
         link: String
         title: String
     }
-
+    type Auth {
+        token: ID!
+        user: User
+    }
+    type Query {
+       me: User
+    }
+  
+  type Mutation {
+    createUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(authors: [String], description: String!, bookId: String!, image: String, link: String, title: String!): User
+    deleteBook(bookId: String!): User
+  }
 `
+module.exports = typeDefs;
